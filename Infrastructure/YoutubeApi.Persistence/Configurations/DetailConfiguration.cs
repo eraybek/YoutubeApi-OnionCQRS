89007a818c8e3a8b6c14032a683155bb3e8a0840
eray.aybek <eray.aybek@EKS-L0342630>
2023-10-09 13:35:41 +0300

    DbContext oluşturuldu.

---
+++ Infrastructure/YoutubeApi.Persistence/Configurations/DetailConfiguration.cs
@@ -0,0 +1,45 @@
+﻿using Bogus;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using YoutubeApi.Domain.Entities;
+
+namespace YoutubeApi.Persistence.Configurations
+{
+    public class DetailConfiguration : IEntityTypeConfiguration<Detail>
+    {
+        public void Configure(EntityTypeBuilder<Detail> builder)
+        {
+            Faker faker = new Faker("tr");
+
+            Detail detail1 = new()
+            {
+                Id = 1,
+                Title = faker.Lorem.Sentence(1),
+                Description = faker.Lorem.Sentence(5),
+                CategoryId = 1, //elektrik kategorisi altındaki detaylar
+                CreatedDate = DateTime.Now,
+                IsDeleted = false
+            };
+            Detail detail2 = new()
+            {
+                Id = 2,
+                Title = faker.Lorem.Sentence(1),
+                Description = faker.Lorem.Sentence(5),
+                CategoryId = 3, 
+                CreatedDate = DateTime.Now,
+                IsDeleted = true
+            };
+            Detail detail3 = new()
+            {
+                Id = 3,
+                Title = faker.Lorem.Sentence(1),
+                Description = faker.Lorem.Sentence(5),
+                CategoryId = 4, 
+                CreatedDate = DateTime.Now,
+                IsDeleted = false
+            };
+            
+            builder.HasData(detail1, detail2, detail3);
+        }
+    }
+}
