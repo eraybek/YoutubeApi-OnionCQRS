b541f2e45aaaccf3c67b678af9f98f6c7f909a9b
eray.aybek <eray.aybek@EKS-L0342630>
2023-10-13 13:38:46 +0300

    Custom AutoMapper yapısı geliştirildi.

---
+++ Core/YoutubeApi.Application/Interfaces/AutoMapper/IMapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubeApi.Application.Interfaces.AutoMapper;
+
+namespace YoutubeApi.Application.Interfaces.AutoMapper
+{
+    public interface IMapper
+    {
+        TDestination Map<TDestination, TSource>(TSource source, string? ignore = null);
+
+        IList<TDestination> Map<TDestination, TSource>(IList<TSource> source, string? ignore = null);
+
+        TDestination Map<TDestination, TSource>(object source, string? ignore = null);
+
+        IList<TDestination> Map<TDestination, TSource>(IList<object> source, string? ignore = null);
+    }
+}
+
+
+// mapper.Map<BrandDto, Brand>(brand)
