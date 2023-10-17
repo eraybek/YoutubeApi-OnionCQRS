1c3924149d542e64edc94c9ed25d904f134c6b3f
eray.aybek <eray.aybek@EKS-L0342630>
2023-10-17 16:56:22 +0300

    Project rules geliştirmesi yapıldı.

--- Core/YoutubeApi.Application/Features/Products/Commands/CreateProduct/CreateProductCommandHandler.cs
+++ Core/YoutubeApi.Application/Features/Products/Commands/CreateProduct/CreateProductCommandHandler.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using YoutubeApi.Application.Features.Products.Rules;
 using YoutubeApi.Application.Interfaces.UnitOfWorks;
 using YoutubeApi.Domain.Entities;
 
@@ -7,14 +8,24 @@ namespace YoutubeApi.Application.Features.Products.Commands.CreateProduct
     public class CreateProductCommandHandler : IRequestHandler<CreateProductCommandRequest, Unit>
     {
         private readonly IUnitOfWork unitOfWork;
+        private readonly ProductRules productRules;
 
-        public CreateProductCommandHandler(IUnitOfWork unitOfWork)
+        public CreateProductCommandHandler(IUnitOfWork unitOfWork, ProductRules productRules)
         {
             this.unitOfWork = unitOfWork;
+            this.productRules = productRules;
         }
 
         public async Task<Unit> Handle(CreateProductCommandRequest request, CancellationToken cancellationToken)
         {
+            IList<Product> products = await unitOfWork.GetReadRepository<Product>().GetAllAsync();
+
+            //if (products.Any(x => x.Title == request.Title))
+            //    throw new Exception("aynı başlıkta ürün olamaz");
+            // clean bir kod değil, single responsibility bozulur.
+
+            productRules.ProductTitleMustNotBeSame(products, request.Title);
+
             Product product = new(request.Title, request.Description, request.BrandId, request.Price, request.Discount);
 
             await unitOfWork.GetWriteRepository<Product>().AddAsync(product);
--- Core/YoutubeApi.Application/Registration.cs
+++ Core/YoutubeApi.Application/Registration.cs
@@ -5,6 +5,8 @@ using YoutubeApi.Application.Exceptions;
 using System.Globalization;
 using MediatR;
 using YoutubeApi.Application.Behaviours;
+using YoutubeApi.Application.Features.Products.Rules;
+using YoutubeApi.Application.Bases;
 
 namespace YoutubeApi.Application
 {
@@ -17,12 +19,26 @@ namespace YoutubeApi.Application
             services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(assembly));
 
             services.AddTransient<ExceptionMiddleware>();
+            //services.AddTransient<ProductRules>();
+            services.AddRulesFromAssemblyContaining(assembly, typeof(BaseRules));
 
             services.AddValidatorsFromAssembly(assembly);
             ValidatorOptions.Global.LanguageManager.Culture = new CultureInfo("tr");
             //services.AddValidatorsFromAssemblyContaining();
 
-            services.AddTransient(typeof(IPipelineBehavior<,>),typeof(FluentValidationBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(FluentValidationBehaviour<,>));
+
+        }
+
+        private static IServiceCollection AddRulesFromAssemblyContaining(this IServiceCollection services, Assembly assembly, Type type)
+        {
+            var types = assembly.GetTypes().Where(t => t.IsSubclassOf(type) && type != t).ToList();
+
+            foreach (var item in types)
+                services.AddTransient(item);
+
+            return services;
+
         }
     }
 }
