9f29677dbefbf868e9ecd89ab3ff72821e2ccb5b
eray.aybek <eray.aybek@EKS-L0342630>
2023-10-24 16:49:29 +0300

    Token Service tamamlandı ve Swagger'da auth arayüzü tanımlandı.

--- Presentation/YoutubeApi.Api/Program.cs
+++ Presentation/YoutubeApi.Api/Program.cs
@@ -3,6 +3,7 @@ using YoutubeApi.Infrastructure;
 using YoutubeApi.Application;
 using YoutubeApi.Mapper;
 using YoutubeApi.Application.Exceptions;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +16,7 @@ builder.Services.AddSwaggerGen();
 
 //Ekledik... ***
 var env = builder.Environment;
+
 builder.Configuration
     .SetBasePath(env.ContentRootPath)
     .AddJsonFile("appsettings.json", optional:false)
@@ -24,6 +26,41 @@ builder.Services.AddPersistence(builder.Configuration);
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
 builder.Services.AddCustomMapper();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Youtube API",
+        Version = "v1",
+        Description = "Youtube API Swagger Client."
+    });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "'Bearer' yazýp boþluk býraktýktan sonra Token'ý girebilirsiniz \r\n\r\n Örneðin: \"Bearer fk*p(b9%dp$k$k3@_=e*h0ju1p1(6@mor0xe*v80ro(h#e^2-n\""
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 //// ***
 
 
