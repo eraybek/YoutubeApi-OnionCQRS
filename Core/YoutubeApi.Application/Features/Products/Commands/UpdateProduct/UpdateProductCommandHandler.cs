1b64af8b354d41ae4cb3dafdc3dddd2532504107
eray.aybek <eray.aybek@EKS-L0342630>
2023-10-26 15:09:50 +0300

    Register işlemleri tamamlandı.

--- Core/YoutubeApi.Application/Features/Products/Commands/UpdateProduct/UpdateProductCommandHandler.cs
+++ Core/YoutubeApi.Application/Features/Products/Commands/UpdateProduct/UpdateProductCommandHandler.cs
@@ -1,21 +1,21 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Http;
 using System.Security.Cryptography.X509Certificates;
+using YoutubeApi.Application.Bases;
 using YoutubeApi.Application.Interfaces.AutoMapper;
 using YoutubeApi.Application.Interfaces.UnitOfWorks;
 using YoutubeApi.Domain.Entities;
 
 namespace YoutubeApi.Application.Features.Products.Commands.UpdateProduct
 {
-    public class UpdateProductCommandHandler : IRequestHandler<UpdateProductCommandRequest, Unit>
+    public class UpdateProductCommandHandler : BaseHandler, IRequestHandler<UpdateProductCommandRequest, Unit>
     {
-        private readonly IUnitOfWork unitOfWork;
-        private readonly IMapper mapper;
 
-        public UpdateProductCommandHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        public UpdateProductCommandHandler(IMapper mapper, IUnitOfWork unitOfWork, IHttpContextAccessor httpContextAccessor) : base(mapper, unitOfWork, httpContextAccessor)
         {
-            this.unitOfWork = unitOfWork;
-            this.mapper = mapper;
+
         }
+
         public async Task<Unit> Handle(UpdateProductCommandRequest request, CancellationToken cancellationToken)
         {
             var product = await unitOfWork.GetReadRepository<Product>().GetAsync(x => x.Id == request.Id && !x.IsDeleted);
